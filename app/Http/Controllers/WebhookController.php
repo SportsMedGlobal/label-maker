ea47aea627ef14667fa794728881a49e51c24049
Ben Kuskopf <kussie@gmail.com>
2016-04-20 15:57:34 +1000

    Github Logic

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -22,7 +22,7 @@ class WebhookController extends Controller
         \Log::debug('Github Webhook', ['response' => $request->json()]);
         $response = json_decode(json_encode($request->json()), true);
 
-        $title = $response['issue']['title'];
+        $title = $response['response']['issue']['title'];
         \Log::info($title);
 
     }
